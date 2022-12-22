6365f70090be05e29f712fd8fc8d64b7d01d0589
Fabrizio Tornatore <torfab94@gmail.com>
2022-12-22 08:41:14 +0100

    :spakles: day 22 very fun, molto contento

--- day22.py
+++ day22.py
@@ -36,8 +36,155 @@ def findWarp(direction, tile, grid):
   if(direction==(0,-1)):
     return (tile[0], max(element[1] for element in grid if element[0]==tile[0]))
 
+# sory for everybody i use cubeshape of my input, if you have different disposition it won't work
+def findWarpInCube(direction, tile, grid):
+  candidateTile=sumTupleValueByValue(direction, tile)
+  if(51<=tile[0]<=100 and 1<=tile[1]<=50):
+    face=1
+  if(101<=tile[0]<=150 and 1<=tile[1]<=50):
+    face=2
+  if(51<=tile[0]<=100 and 51<=tile[1]<=100):
+    face=3
+  if(51<=tile[0]<=100 and 101<=tile[1]<=150):
+    face=4
+  if(1<=tile[0]<=50 and 101<=tile[1]<=150):
+    face=5
+  if(1<=tile[0]<=50 and 151<=tile[1]<=200):
+    face=6
+
+
+  # destra
+  # if(face==1 and direction ==(1,0)):#absurd
+  #   return (101, candidateTile[1])
+
+  #sinistra
+  if(face==1 and direction ==(-1,0)):
+    rotate("L")
+    rotate("L")
+    return (1, 151-candidateTile[1]), ["R", "R"] #1=150 50=101
+
+  #su
+  if(face==1 and direction ==(0,-1)):
+    rotate("R")
+    return ((1, tile[0]+100)), ["L"] #0=151
+
+  #giu
+  # if(face==1 and direction ==(0,1)): #absurd
+  #   return (51, tile[1])
+  
+  #destra
+  if(face==2 and direction ==(1,0)):
+    rotate("L")
+    rotate("L")
+    return (100, 151-candidateTile[1]), ["R", "R"]
+
+  #sinistra
+  # if(face==2 and direction ==(-1,0)): #absurd
+  #   return (1, 150-candidateTile[1]) 
+
+  #su
+  if(face==2 and direction==(0,-1)):
+    return (candidateTile[0]-100, 200), [] #0=151
+
+  #giu
+  if(face==2 and direction==(0,1)): 
+    rotate("R")
+    return (100, candidateTile[0]-50), ["L"]
+
+  #destra
+  if(face==3 and direction ==(1,0)):
+    rotate("L")
+    return (candidateTile[1]+50, 50), ["R"]
+
+  # sinistra
+  if(face==3 and direction ==(-1,0)):
+    rotate("L")
+    return (candidateTile[1]-50, 101), ["R"]
+
+  #su
+  # if(face==2 and direction==(0,-1)): #absurd
+  #   return (candidateTile[0]-100, 200) 
+
+  #giu
+  # if(face==2 and direction==(0,1)): #absurd
+  #   return (100, candidateTile[0]-50)
+
+  #destra
+  if(face==4 and direction ==(1,0)):
+    rotate("L")
+    rotate("L")
+    return (150, 151-candidateTile[1]), ["R", "R"]
+
+
+  # sinistra
+  # if(face==4 and direction ==(-1,0)): 
+  #   return (candidateTile[1]-50, 101) 
+
+  #su
+  # if(face==4 and direction==(0,-1)): #absurd
+  #   return (candidateTile[0]-100, 200) 
+
+  #giu
+  if(face==4 and direction==(0,1)):
+    rotate("R")
+    return (50, candidateTile[0]+100), ["L"]
+
+  #destra
+  # if(face==5 and direction ==(1,0)): #absurd
+  #   return (150, 151-candidateTile[1])
+
+  # sinistra
+  if(face==5 and direction ==(-1,0)): 
+    rotate("R")
+    rotate("R")
+    return (51, 151-candidateTile[1]), ["L", "L"]
+
+  #su
+  if(face==5 and direction==(0,-1)):
+    rotate("R")
+    return (51, candidateTile[0]+50), ["L"]
+
+  #giu
+  # if(face==5 and direction==(0,1)): #absurd
+  #   return (50, candidateTile[1]+100)
+
+
+  #destra
+  if(face==6 and direction ==(1,0)):
+    rotate("L") 
+    return (candidateTile[1]-100, 150), ["R"]
+
+  # sinistra
+  if(face==6 and direction ==(-1,0)): 
+    rotate("L")
+    return (candidateTile[1]-100, 1), ["R"]
+
+  #su
+  # if(face==5 and direction==(0,-1)): #absurd
+  #   return (51, candidateTile[0]+50) 
+
+  #giu
+  if(face==6 and direction==(0,1)): #absurd
+    return (candidateTile[0]+100, 1), []
+
+
+
 def stamp(tile, grid):
   grid[tile]=orientationVector[0]
+
+def goForwardInCube(currentPosition, steps, grid):
+  for element in range(steps):
+    candidatePosition=sumTupleValueByValue(currentPosition, directionVector[0])
+    arrayOfRotation=[]
+    if(grid.get(candidatePosition)==None):
+      candidatePosition, arrayOfRotation=findWarpInCube(directionVector[0], currentPosition, grid)
+    if(grid[candidatePosition]=="#"):
+      for rotation in arrayOfRotation:
+        rotate(rotation)
+      return currentPosition
+    currentPosition=candidatePosition
+    stamp(currentPosition, grid)
+  return currentPosition
 
 def goForward(currentPosition, steps, grid):
   for element in range(steps):
@@ -74,7 +221,9 @@ def calculateResult(tile, orientation):
     orientationValue=3
   return (tile[1]*1000+tile[0]*4+orientationValue)
 
-def solve():
+def solve(forwardFunction):
+  while(directionVector[0]!=(1,0)):
+    rotate("R")
   rows=getAocInput(day)
   grid, arrayOfCommands=comprehension(rows)
   currentPosition=(min(element[0] for element in grid if element[1]==1), 1)
@@ -84,8 +233,9 @@ def solve():
       rotate(command)
       stamp(currentPosition, grid)
     else:
-      currentPosition=goForward(currentPosition, int(command), grid)
+      currentPosition=forwardFunction(currentPosition, int(command), grid)
   return calculateResult(currentPosition, orientationVector[0])
 
 
-print(solve())
+print(solve(goForward))
+print(solve(goForwardInCube))
