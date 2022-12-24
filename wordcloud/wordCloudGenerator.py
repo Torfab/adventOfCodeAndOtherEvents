9f55d2f028556eb4f23882b8726a3575f7abd586
Torfab <torfab94@gmail.com>
2022-12-24 04:13:41 +0100

    :sparkles: updated word cloud generator
    now you can use a relative path from a file.
    now you can decide to avoid pruning not interesting words.
    now you can decide at how much frequency you want to stop limiting an unbalance image

--- wordcloud/wordCloudGenerator.py
+++ wordcloud/wordCloudGenerator.py
@@ -8,7 +8,7 @@ from PIL import Image
 from wordcloud import WordCloud
 
 
-def getFrequencyDictForText(sentence):
+def getFrequencyDictForText(sentence, pruneNotUsefulWords=True, tooManyOccurance=7):
   if(not sentence):
     print("absurd")
     return
@@ -17,10 +17,11 @@ def getFrequencyDictForText(sentence):
   # making dict for counting frequencies
   sentence=re.sub('[^A-Za-z]+', ' ', sentence)
   for text in sentence.split(" "):
-    if re.match("in|print|def|for|dict|def|idx|range|return|element|splitted|len|result|if|rows|solve|append|and|split|int|else|import|utilities|from|continue|sort|break|pop|or|while|elif|float", text):
-      continue
+    if(pruneNotUsefulWords):
+      if re.match("the|a|of|in|print|def|for|dict|def|idx|range|return|element|splitted|len|result|if|rows|solve|append|and|split|int|else|import|utilities|from|continue|sort|break|pop|or|while|elif|float", text):
+        continue
     val = tmpDict.get(text, 0)
-    tmpDict[text.lower()] = min(val + 1, 7)
+    tmpDict[text.lower()] = min(val + 1, tooManyOccurance)
   for key in tmpDict:
     fullTermsDict.add(key, tmpDict[key])
   return fullTermsDict
@@ -37,16 +38,22 @@ def generate(dictOfText, arrayOfImages):
     print("generated from "+element)
   print("completed")
 
-def getTextFromFiles(path):
+def getTextFromFiles(path, custom=False):
   realText=''
-  for day in range(1,23):
-    file=path+"day"+str(day)+".py"
-    text = open(os.path.dirname(__file__)+file, "r")
+  if(not custom):
+    for day in range(1,23):
+      file=path+"day"+str(day)+".py"
+      text = open(os.path.dirname(__file__)+file, "r")
+      data=text.read()
+      text.close()
+      realText = realText+data
+  else:
+    text = open(path, "r")
     data=text.read()
     text.close()
-    realText = realText+data
+    realText = realText+data     
   return realText
 
 
 # generate(getFrequencyDictForText(getTextFromFiles("/../")),["santa.jpg", "elephants.jpg", "tree.png"])
-generate(getFrequencyDictForText(getTextFromFiles("/../")),["tree.png"])
+generate(getFrequencyDictForText(getTextFromFiles("allChallenges.txt", True), True, 7),["santa.jpg", "elephants.jpg", "tree.png"])
