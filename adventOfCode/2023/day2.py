efb2c82d3308c093efaa0daf546d198e4b1b6a27
Fabrizio Tornatore <torfab94@gmail.com>
2023-12-04 13:42:18 +0100

    :sparkles: day2 2023

---
+++ adventOfCode/2023/day2.py
@@ -0,0 +1,46 @@
+from utility import *
+
+aLimits={"red": 12, "green": 13, "blue": 14}
+
+def solveA(row):
+  parsedRow=row.split(": ")
+  rounds=parsedRow[1].split("; ")
+  isValidGame=True
+  for singleRound in rounds:
+    estrazioni=singleRound.split(", ")
+
+    for singleEstrazione in estrazioni:
+      singleEstrazioneParsed=singleEstrazione.split(" ")
+      if (aLimits[singleEstrazioneParsed[1]]<int(singleEstrazioneParsed[0])):
+        isValidGame=False
+  
+  if(isValidGame):
+    return int(parsedRow[0].split(" ")[1])
+  return 0
+
+def solveB(row):
+  parsedRow=row.split(": ")
+  rounds=parsedRow[1].split("; ")
+  minCubes={"red":0, "blue": 0, "green": 0}
+  
+  for singleRound in rounds:
+    estrazioni=singleRound.split(", ")
+    for singleEstrazione in estrazioni:
+      singleEstrazioneParsed=singleEstrazione.split(" ")
+      if (minCubes[singleEstrazioneParsed[1]]<int(singleEstrazioneParsed[0])):
+        minCubes[singleEstrazioneParsed[1]]=int(singleEstrazioneParsed[0])
+  
+  return minCubes["red"]*minCubes["blue"]*minCubes["green"]
+
+def solve(part):
+  rows=getOldAocInput(2)
+  result=0
+  for row in rows:
+    if(part=="a"):
+      result=result+solveA(row)
+    elif(part=="b"):
+      result=result+solveB(row)
+  return result
+
+print(solve("a"))
+print(solve("b"))
