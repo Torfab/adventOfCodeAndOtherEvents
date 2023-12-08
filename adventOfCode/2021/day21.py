8e774ab481063e5aa5cce18484d5f466d20b4c4d
Torfab <torfab94@gmail.com>
2023-12-08 03:02:58 +0100

    :sparkles: day21 2021 part b

--- adventOfCode/2021/day21.py
+++ adventOfCode/2021/day21.py
@@ -21,17 +21,57 @@ def solve():
     positions[player]=step(positions[player], currentDice)
     currentDice=(currentDice+3)%100
     values[player]=values[player]+positions[player]+1
-    # print(diceRolls, currentDice, values)
-    # input("step")
     if(values[player]>=1000):
       break
     player=(player+1)%2
-
-  # print(values)
-  # print("rolls", diceRolls)
-
   result=min(values)*diceRolls*3
 
   return result
 
+
+def buildQuantumDices():
+  quantumDices={}
+  for d1 in range(1,4):
+    for d2 in range(1,4):
+      for d3 in range(1,4):
+        dTot=d1+d2+d3
+        if(quantumDices.get(dTot)==None):
+          quantumDices[dTot]=1
+        else:
+          quantumDices[dTot]=quantumDices[dTot]+1
+  return quantumDices
+
+def solveB():
+  rows=getOldAocInput(21)
+  positions=parseRows(rows)
+  wins=[0,0]
+
+  quantumDices=buildQuantumDices()
+  states={(positions[0], positions[1], 0, 0): 1}
+  turn=1
+  while(len(states)>0):
+    newStates={}
+    turn=(turn+1)%2
+    notTurn=(turn+1)%2
+    
+    for k, v in states.items():
+      for d,q in quantumDices.items():
+        newTuple=[0,1,2,3]
+        newTuple[turn]=(k[turn]+d)%10
+        newTuple[notTurn]=k[notTurn]
+        scoreTurn=k[turn+2]+newTuple[turn]+1
+        if(scoreTurn>=21):
+          wins[turn]=wins[turn]+q*v
+          continue
+        newTuple[turn+2]=scoreTurn
+        newTuple[notTurn+2]=k[notTurn+2]
+        newTuple=tuple(newTuple)
+        if(newStates.get(newTuple)==None):
+          newStates[newTuple]=q*v
+        else:
+          newStates[newTuple]=newStates[newTuple]+q*v
+    states=newStates
+  return max(wins)
+
 print(solve())
+print(solveB())
