28ac98c5d856198a213880d47d2c06fec204c372
Torfab <torfab94@gmail.com>
2024-12-06 20:35:58 +0100

    :zap: aoc 2024 day6 optimization

--- adventOfCode/2024/day6.py
+++ adventOfCode/2024/day6.py
@@ -2,57 +2,60 @@ from utility import *
 
 directions=[(0,-1),(1,0),(0,1),(-1,0)]
 
-def guardLoopA(currentPosition, walls, limits):
-  maxX=limits[0]
-  maxY=limits[1]
-  grid={}
-  currentDirectionIdx=0
-  while(0<=currentPosition[0]<=maxX and 0<=currentPosition[1]<=maxY):
-    grid[currentPosition]="█"
-    tentative=sumTupleValueByValue(currentPosition, directions[currentDirectionIdx])
-    while(tentative in walls):
-      currentDirectionIdx=(currentDirectionIdx+1)%len(directions)
-      tentative=sumTupleValueByValue(currentPosition, directions[currentDirectionIdx])
-    currentPosition=tentative
-  return list(grid.keys())
+def guardLoopA(currentPosition, walls, outside):
 
-def guardLoopB(currentPosition, walls, limits):
-  maxX=limits[0]
-  maxY=limits[1]
+  gridSet=set()
   currentDirectionIdx=0
-  visited=set()
-  while(0<=currentPosition[0]<=maxX and 0<=currentPosition[1]<=maxY):
-    if (currentPosition, currentDirectionIdx) in visited:
-      return 1
-    visited.add((currentPosition, currentDirectionIdx))
+  while(currentPosition not in outside):
+    gridSet.add(currentPosition)
     tentative=sumTupleValueByValue(currentPosition, directions[currentDirectionIdx])
     while(tentative in walls):
       currentDirectionIdx=(currentDirectionIdx+1)%len(directions)
       tentative=sumTupleValueByValue(currentPosition, directions[currentDirectionIdx])
     currentPosition=tentative
+  return gridSet
+
+def guardLoopB(currentPosition, walls, outside):
+  currentDirectionIdx=0
+  d=directions[currentDirectionIdx]
+  visited=set()
+  while(currentPosition not in outside):
+    tentative=sumTupleValueByValue(currentPosition, d)
+    if tentative not in walls:
+      currentPosition=tentative
+    else:
+      currentDirectionIdx=(currentDirectionIdx+1)%len(directions)
+      d=directions[currentDirectionIdx]  
+      if (currentPosition, currentDirectionIdx) in visited:
+        return 1
+      visited.add((currentPosition, currentDirectionIdx))
   return 0
 
-def solve():
-  rows=getOldAocInput(6)
-  grid, maxX, maxY=buildGrid(rows)
-  currentPosition=[k for k,v in grid.items() if v=="^"][0]
-  walls=set([k for k,v in grid.items() if v=="#"])
-  return  len(guardLoopA(currentPosition, walls, (maxX, maxY)))
+def buildOutside(limits):
+  outside=set()
+  for x in range(-1,limits[0]+2):
+    outside.add((x,-1))
+    outside.add((x,limits[1]+1))
+  for y in range(-1,limits[1]+2):
+    outside.add((-1,y))
+    outside.add((limits[0]+1,y))
+  return outside
 
-def solveB():
+def solve(part):
   rows=getOldAocInput(6)
   grid, maxX, maxY=buildGrid(rows, None)
   currentPosition=[k for k,v in grid.items() if v=="^"][0]
   walls=set([k for k,v in grid.items() if v=="#"])
-  grid[currentPosition]="."
-  possibleWalls=guardLoopA(currentPosition, walls, (maxX, maxY))
-
+  outside=buildOutside((maxX,maxY))
+  possibleWalls=guardLoopA(currentPosition, walls, outside)
+  if(part=="a"):
+    return len(possibleWalls)
+  possibleWalls.remove(currentPosition)
   ris=0
-  for elementIdx in range(len(possibleWalls)):
-    ris=ris+guardLoopB(currentPosition, walls.union(set([possibleWalls[elementIdx]])), (maxX, maxY))
+  for element in possibleWalls:
+    ris=ris+guardLoopB(currentPosition, walls.union(set([element])), outside)
   return ris
 
-print(solve())
-print(solveB())
-# print(solveB())
+print(solve("a"))
+print(solve("b"))
 
