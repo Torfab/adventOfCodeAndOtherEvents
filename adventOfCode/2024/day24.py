edba7578981f3ce6350a138c8279cb420541ebc2
Torfab <torfab94@gmail.com>
2024-12-25 01:34:54 +0100

    :sparkles: aoc 2024 day 24 non tailored solution

--- adventOfCode/2024/day24.py
+++ adventOfCode/2024/day24.py
@@ -1,4 +1,4 @@
-from utilityz import *
+from utility import *
 
 # Not generic solution for part 2 but tailored on my input, it's a good error detection tho 
 # After i detected an error I wrote the part of code that solved my input, i did it 4 times)
@@ -65,7 +65,7 @@ def solve():
 
   return fromBinaryToInteger(ris)
 
-
+#DEPRECATED, but i keep it here for legacy
 def startAnswer(start, operations):
   currentOperation=next((operation for operation in operations if operation["operator"]=="XOR" and operation["firstOperand"]=="x"+padNumberString(start)), None)
   if currentOperation["targetRegister"]!="z"+padNumberString(start):
@@ -85,7 +85,8 @@ def padNumberString(num):
   if(len(num)==1):
     return "0"+num
   return num
-  
+
+#DEPRECATED, but i keep it here for legacy
 def fixThingsWithZ(temp, operations, sumTemp,  currentOperation, carriersArray, currentIndex, risArray):
   tempIdx=int(temp[1:])
   if tempIdx==currentIndex:
@@ -117,9 +118,10 @@ def fixThingsWithZ(temp, operations, sumTemp,  currentOperation, carriersArray, 
     risArray.append(realDeal)
     return realDeal
   else:
-    # print("non posso farcela")
+    # print("It shouldn't happen")
     return None
 
+#DEPRECATED, but i keep it here for legacy
 def findCorrectZ(temp, currentOperation, currentIdx, operations, risArray):
   z="z"+padNumberString(currentIdx)
 
@@ -132,6 +134,7 @@ def findCorrectZ(temp, currentOperation, currentIdx, operations, risArray):
   # print("tuttoapposto")
   return z
 
+#DEPRECATED, but i keep it here for legacy
 def findTheRealOtherAnswer(sumTemp, currentCarrier, operations,risArray):
   newCurrentOperation=next((operation for operation in operations if operation["operator"]=="XOR" and operation["firstOperand"]==currentCarrier ), None)
   if newCurrentOperation==None:
@@ -150,14 +153,14 @@ def findTheRealOtherAnswer(sumTemp, currentCarrier, operations,risArray):
   risArray.append(realSumTemp)
   return realSumTemp, sumTemp
 
-
+#DEPRECATED, but i keep it here for legacy
 def continueAnswer(currentIndex, currentCarrier, operations, carriersArray, risArray):
   # A binary sum use the following 5 operations to complete a digit:
   # 1 A xoR B = sumTemp
-  # 2 A and B = CoutTemp
-  # 3 Cout(-1) xor sumTemp = Z
-  # 4 Cout(-1) and sumTemp = CoutYemp
-  # 5 CoutTemp or CoutYemp = Cout(0) -> it will be next cout(-1)
+  # 2 A and B = carrierTemp
+  # 3 lastCarrier xor sumTemp = Z
+  # 4 lastCarrier and sumTemp = carrierYemp
+  # 5 carrierTemp or carrierYemp = currentCarriage (considering it's last operation i save it directly as last carriage of next iteration)
   # Technically A, B and Cout(-1) can be swapped intercycle but the input use these in this order, so it's ok
 
   currentOperation=next((operation for operation in operations if operation["operator"]=="XOR" and operation["firstOperand"]=="x"+padNumberString(currentIndex)), None)
@@ -216,6 +219,7 @@ def continueAnswer(currentIndex, currentCarrier, operations, carriersArray, risA
 
   return currentCarrier
 
+#DEPRECATED, but i keep it here for legacy
 def solveB():
   rows=getOldAocInput(24)
   registers, operations=parseRows(rows)
@@ -236,11 +240,93 @@ def solveB():
   risArray.sort()
   return ",".join(risArray)
 
+# New approach, we do not have to swap and fix the sum, therefore we can just improve our detection mechanism and that's it.
+# Probably it will be less comprehensive, but surely is less tailored on my input. I think it's comprehensive enough tho.
+# First thing i checked, there is no overwrite register and there are no redherring operations. 
+# All the operations make sense and are not redundant so we can use the occurances of them to find result
+def solveC():
+  rows=getOldAocInput(24)
+  _, operations=parseRows(rows)
+
+  # Idea behind is still having those operations to do in order, in particular sumTemp appear twice and carrierTemp once
+  # 1 A xoR B = sumTemp
+  # 2 A and B = carrierTemp
+  # 3 lastCarrier xor sumTemp = Z
+  # 4 lastCarrier and sumTemp = carrierYemp
+  # 5 carrierTemp or carrierYemp = currentCarriage (considering it's last operation i save it directly as last carriage of next iteration)
+
+  #First thing first, we know that left part of operations are good, so i save which register appear twice and which once
+  ris=[]
+  dictCounter={}
+  for operation in operations:
+    dictCounter[operation["firstOperand"]]=dictCounter.get(operation["firstOperand"], 0)+1
+    dictCounter[operation["secondOperand"]]=dictCounter.get(operation["secondOperand"], 0)+1
+
+  # The only iterations that do not work as explained are the very first one (there is no carriage) 
+  # And the very last one (there is no things to sum on carriage)
+  # Input looks tailored to not have any problem on those, if you find you have too few elements you can try to check them
+  # Anyway we delete from operations the firsts and the lasts, firsts start with x00, and last is with z45 (or last element)
+  deleteFirst=[operation for operation in operations if operation["firstOperand"]=="x00"]
+  for operation in deleteFirst:
+    operations.remove(operation)
+  listZ=[operation for operation in operations if operation["targetRegister"][0]=="z"]
+  lastElement="z"+padNumberString(len(listZ))
+  deleteLast=next(operation for operation in operations if operation["targetRegister"]==lastElement)
+  operations.remove(deleteLast)
+
+  #Now we consider the different scenario just looking at what we expect
+  #sumTemps should appear twice, carrierTemp once, lastCarrier twice and carrierYemp once.
+  #Xor element with x/y register should write NO Z
+  #Xor element without x/y register should write Z elements
+  sumTemps=[]
+  carrierTemp=[]
+  lastCarriers=[]
+  carrierYemp=[]
+  shouldBeZed=[]
+  shouldNotBeZed=[]
+  for operation in operations:
+    if operation["operator"]=="XOR" and operation["firstOperand"][0]=="x":
+      sumTemps.append(operation["targetRegister"])
+    elif operation["operator"]=="AND" and operation["firstOperand"][0]=="x":
+      carrierTemp.append(operation["targetRegister"])
+    elif operation["operator"]=="XOR" and operation["firstOperand"][0]!="x":
+      shouldBeZed.append(operation["targetRegister"])
+    elif operation["operator"]=="AND" and operation["firstOperand"][0]!="x":
+      carrierYemp.append(operation["targetRegister"])
+    elif operation["operator"]=="OR":
+      lastCarriers.append(operation["targetRegister"])
+    else:
+      shouldNotBeZed.append(operation["targetRegister"])
+
+  #now that i've all the datastructures filled we just do the checks
+  for element in shouldBeZed:
+    if element[0]!="z":
+      ris.append(element)
+  for element in shouldNotBeZed:
+    if element[0]=="z":
+      ris.append(element)
+  for element in carrierTemp:
+    if dictCounter.get(element)!=1:
+      ris.append(element)
+  for element in sumTemps:
+    if dictCounter.get(element)!=2:
+      ris.append(element)
+  for element in lastCarriers:
+    if dictCounter.get(element)!=2:
+      ris.append(element)
+  for element in carrierYemp:
+    if dictCounter.get(element)!=1:
+      ris.append(element)
+
+  #put in order and send answer
+  ris.sort()
+  return ",".join(ris)
+
 print(solve())
-print(solveB())
+print(solveC())
 
-# def timeElapse():
-#   print(solve())
-#   print(solveB())
+def timeElapse():
+  print(solve())
+  print(solveC())
 
-# print(evaluateTime(timeElapse))
+print(evaluateTime(timeElapse))
