d78c44f1265abc8d67d94d713f5bbd731259e2de
Torfab <torfab94@gmail.com>
2024-12-27 02:41:54 +0100

    :sparkles: day19 aoc 2020 HARD

--- adventOfCode/2020/day19.py
+++ adventOfCode/2020/day19.py
@@ -1,4 +1,8 @@
-from utilityz import *
+from utility import *
+
+# One of harder days for me, i didn't want to use a regex and i didn't know how to really handle the problem
+# After several ideas, i went to the idea of path finding between result space.
+# Solution is slow but is generic, it has no problem handling any kind of formal language with this kind of rules.
 
 rules={}
 
@@ -27,6 +31,7 @@ def parseRows(rows):
       messages.append(row)
   return rules, messages
 
+#DEPRECATED it's here only for legacy, this solves part 1 but i used another strategy for part 1/2
 def findOkRules(ruleKey, idx, message):
   if(idx==len(message)):
     return -1
@@ -46,7 +51,6 @@ def findOkRules(ruleKey, idx, message):
     while(atWhichPoint<len(possibility)):
       idx=findOkRules(possibility[atWhichPoint], idx, message)
       if(idx is True):
-        print(atWhichPoint, len(possibility)-1)
         if(atWhichPoint==(len(possibility)-1)):
           return True
         return -1
@@ -56,10 +60,10 @@ def findOkRules(ruleKey, idx, message):
     if idx!=-1:
       solutions.append(idx)
   if len(solutions)>0:
-    print(solutions)
     return solutions[0]
   return -1
 
+#DEPRECATED it's here only for legacy, this solves part 1 but i used another strategy for part 1/2
 def checkMessage(message):
   resultIdx=findOkRules(0, 0, message)
   if(resultIdx==True):
@@ -72,8 +76,41 @@ def checkMessage(message):
     return False
   
 
+def checkMessageB(message):
+  idxToCheck=0
+  startPath=rules[0][0]
+  border=[([(startPath, 0)], idxToCheck, "F")]
+  lenMessage=len(message)
+  while(len(border)>0):
+    fullPath, idxToCheck, direction=border.pop()
+    if len(fullPath)==0:
+      if(idxToCheck==lenMessage):
+        return True
+      else:
+        continue
+    if idxToCheck==lenMessage and direction=="F":
+      continue
+    lastElementOfPath, choice=fullPath[-1]
+    if(isinstance(lastElementOfPath, str)):
+      if lastElementOfPath==message[idxToCheck]:
+        border.append((fullPath[:len(fullPath)-1], idxToCheck+1, "B"))
+      continue
+    if direction=="F":
+      for element in rules[lastElementOfPath[choice]]:
+        border.append((fullPath+[(element,0)], idxToCheck, "F"))
+      continue
+    if direction=="B":
+      if(choice<len(lastElementOfPath)-1):
+        fullPath[-1]=(fullPath[-1][0], choice+1)
+        for element in rules[lastElementOfPath[choice+1]]:
+          border.append((fullPath+[(element,0)], idxToCheck, "F"))
+      else:
+        border.append((fullPath[:len(fullPath)-1], idxToCheck, "B"))
+  return False
 
-def solve():
+# DEPRECATED it's here only for legacy, this solves part 1 but i used another strategy for part 1/2
+# It's a bit faster tho
+def solveDEPRECATED():
   rows=getOldAocInput(19)
   global rules
   rules, messages=parseRows(rows)
@@ -87,27 +124,25 @@ def updateRules():
   global rules
   rules[8]=[[42],[42,8]]
   rules[11]=[[42, 31], [42,11,31]]
-  print(rules)
 
-def solveB():
+def solve(part):
   rows=getOldAocInput(19)
   global rules
   rules, messages=parseRows(rows)
-  updateRules()
+  if part=="b":
+    updateRules()
   ris=0
   for message in messages:
-    if(checkMessage(message)):
-      print(message)
+    if(checkMessageB(message)):
       ris=ris+1
   return ris
 
-
-# print(solve())
-print(solveB())
+print(solve("a"))
+print(solve("b"))
 
 # def timeElapse():
-#   print(solve())
-#   print(solveB())
+#   print(solve("a"))
+#   print(solve("b"))
 
 # print(evaluateTime(timeElapse))
 
