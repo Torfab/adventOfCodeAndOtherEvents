cd4d8b27d825ae0e07a509a7f1f2382141eb595a
Torfab <torfab94@gmail.com>
2025-06-17 11:27:48 +0200

    :sparkles: s1 q1 completed

--- everybodycodes/EoE/q1.py
+++ everybodycodes/EoE/q1.py
@@ -1,4 +1,4 @@
-from utilityz import *
+from utility import *
 
 def parseRows(rows):
 
@@ -12,7 +12,7 @@ def parseRows(rows):
     newRows.append(newItem)
   return newRows
 
-def eni(N, exp, mod):
+def eni1(N, exp, mod):
   result=[]
   start=1
   for _ in range(exp):
@@ -20,7 +20,7 @@ def eni(N, exp, mod):
     result.insert(0, str(start))
   return int("".join(result))
 
-def eniSmart(N, exp, mod):
+def eni2(N, exp, mod):
   chain=dict()
   chainResult=["1"]
   start=1
@@ -30,7 +30,6 @@ def eniSmart(N, exp, mod):
     chain[start]=newStart
     chainResult.append(str(newStart))
     if(chain.get(newStart)!=None):
-      
       if(newStart!=1):
         indexFound=chainResult.index(str(newStart))
         # print("index", indexFound, N, chain)
@@ -40,16 +39,59 @@ def eniSmart(N, exp, mod):
       # print(chainResult)
       break
     start=newStart
-  lenchain=len(chain)
-  oldExp=exp
-  exp=exp-indexFound
-  exp=exp%lenchain
+  lenchain=len(chainResult)
+  if(exp<=lenchain):
+    result=""
+    for i in range(exp,0,-1):
+      result=result+chainResult[i]
+    return int(result)
+  tempExp=exp-indexFound
+  tempExp=tempExp%lenchain
   result=""
-  for _ in range(min(oldExp, 5)):
-    exp=exp-1
-    result=result+chainResult[exp%len(chainResult)]
+  for _ in range(min(exp, 5)):
+    result=result+chainResult[tempExp%len(chainResult)]
+    tempExp=tempExp-1
   return int(result)
   
+def eni3(N, exp, mod):
+  chain=dict()
+  chainResult=[]
+  start=1
+  indexFound=0
+  result=0
+  for _ in range(exp):
+    newStart=(start*N)%mod
+    chain[start]=newStart
+    chainResult.append(newStart)
+    if(chain.get(newStart)!=None):
+      if(newStart!=1):
+        indexFound=chainResult.index(newStart)
+        for element in range(1, indexFound):
+          result=result+chainResult[element]
+        chainResult=chainResult[indexFound:]
+        chainResult.pop()
+      break
+    start=newStart
+  lenchain=len(chainResult)
+  if(exp<=lenchain):
+    result=""
+    for i in range(exp,0,-1):
+      result=result+chainResult[i]
+    return int(result)
+  tempExp=exp-indexFound
+  loops=tempExp//lenchain
+  for element in chainResult:
+    result=result+loops*element
+  tempExp=tempExp%lenchain
+  for element in range(tempExp):
+    result=result+chainResult[element]
+  return result
+
+# print(eni3(2,7,5)) 
+# print(eni3(3,8,16))
+# print(eni3(4,3000,110)) 
+# print(eni3(4,14000,110)) 
+# print(eni3(6,15000,110)) 
 
 def solve(theFunction):
   rows=openFile("raw.txt")
@@ -61,10 +103,10 @@ def solve(theFunction):
     second=theFunction(row["B"], row["Y"], row["M"])
     third=theFunction(row["C"], row["Z"], row["M"])
     maxResult=max(maxResult, first+second+third)
-    print(first, second, third)
     i=i+1
     
   return maxResult
 
-# print(solve(eni))
-print(solve(eniSmart))
+# print(solve(eni1))
+# print(solve(eni2))
+print(solve(eni3))
