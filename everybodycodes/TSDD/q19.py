c4bb6d6c36c7677c0af1cd470ff8d35e7e9276a3
Torfab <torfab94@gmail.com>
2025-12-01 01:30:05 +0100

    ec2 tsdd q19 in progress

---
+++ everybodycodes/TSDD/q19.py
@@ -0,0 +1,61 @@
+from utility import *
+
+def parseRows(rows):
+  obstacles=[]
+  for row in rows:
+    rowSplitted=[int(x) for x in row.split(",")]
+    obstacles.append(tuple(rowSplitted))
+
+  return obstacles
+
+
+def solve():
+  rows=openFile("raw.txt")
+  obstacles=parseRows(rows)
+  currentPositions=set()
+  currentPositions.add((0,0)) #height and flaps
+  horizontal=0
+  for obstacle in obstacles:
+    distanceFromZero, heightStart, height=obstacle
+    distance=distanceFromZero-horizontal
+    heightEnd=heightStart+height
+
+    # Data la distanza dispari e l'altezza corrente pari io posso raggiungere solo numeri dispari
+    # Se la distanza fosse pari e l'altezza corrente pario avrei raggiungo solo numeri pari
+    # distanza dispari e altezza dispari num pari
+    # distanza pari e altezza dispari num dispari
+    print(currentPositions)
+    if distance+next(iter(currentPositions))[0]%2==0:
+      if heightStart%2==0:
+        offSet=0
+      else:
+        offSet=1
+    else:
+      if heightStart%2==0:
+        offSet=1
+      else:
+        offSet=0      
+
+    realHeight=heightStart+offSet
+    reachablePoints=[]
+    while(realHeight<heightEnd):
+      reachablePoints.append(realHeight)
+      realHeight=realHeight+2
+    print(reachablePoints)
+    newPositions=set()
+    for position in currentPositions:
+      currentHeight, flapNumbers=position
+      for reachablePoint in reachablePoints:
+        flapNumbers=flapNumbers+(distance+reachablePoint-currentHeight)//2
+        if flapNumbers<0 or flapNumbers>distance:
+          continue
+        newPositions.add((reachablePoint, flapNumbers))
+    currentPositions=newPositions
+    horizontal=distanceFromZero
+
+
+
+  return currentPositions
+
+
+print(solve())
