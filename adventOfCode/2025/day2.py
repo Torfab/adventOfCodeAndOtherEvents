f94873b5873af97640f041dc96a2696f2bb3a53c
Torfab <torfab94@gmail.com>
2025-12-02 10:54:35 +0100

    :sparkles: aoc2025 day2

---
+++ adventOfCode/2025/day2.py
@@ -0,0 +1,38 @@
+def parseRows(rows):
+  ranges=rows[0].split(",")
+  toCheck=[]
+  for range in ranges:
+    realRange=range.split("-")
+    toCheck.append((int(realRange[0]), int(realRange[1])))
+  return toCheck
+
+
+def isValid(idx):
+  idx=str(idx)
+  lenId=len(idx)
+  if (idx[:lenId//2]==idx[lenId//2:]):
+    return False
+  return True
+
+def isValid2(idx):
+  idx=str(idx)
+  lenId=len(idx)
+  for i in range(1,lenId):
+    if(lenId%i==0):
+      if idx[:i]*(lenId//i)==idx:
+        return False
+  return True
+
+def solve(f):
+  raw=getOldAocInput(2)
+  toCheck=parseRows(raw)
+  count=0
+  for element in toCheck:
+    for idx in range(element[0], element[1]+1):
+      if(not f(idx)):
+        count=count+idx
+  return count
+  
+
+print(solve(isValid))
+print(solve(isValid2))
