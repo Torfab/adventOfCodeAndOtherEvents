a1990d6eb4c85844dc9b161a77c27261599e4274
Torfab <torfab94@gmail.com>
2025-12-02 10:12:25 +0100

    :sparkles: aoc2025 day1

---
+++ adventOfCode/2025/day1.py
@@ -0,0 +1,47 @@
+import * from utility
+
+def parseRows(rows):
+  orders=[]
+  for row in rows:
+    direction=row[0]
+    quantity=int(row[1:])
+    orders.append((direction, quantity))
+  return orders
+
+def solve():
+  rows=getOldAocInput(1)
+  orders=parseRows(rows)
+  size=100
+  current=50
+  count=0
+  for order in orders:
+    if order[0]=="R":
+      direction= 1
+    if order[0]=="L":
+      direction= -1
+    current=(current+direction*order[1])%size
+    if current==0:
+      count=count+1
+  return count
+    
+def solve2():
+  orders=parseRows(raw)
+  size=100
+  current=50
+  count=0
+  for order in orders:
+    if order[0]=="R":
+      direction= 1
+    if order[0]=="L":
+      direction= -1
+    count=count+order[1]//100
+    realOrder=order[1]%size
+    newPosition=(current+direction*realOrder)
+    if current!=0:
+      if newPosition<=0 or newPosition>=100:
+        count=count+1
+    current=newPosition%size
+  return count
+
+print(solve())
+print(solve2())
